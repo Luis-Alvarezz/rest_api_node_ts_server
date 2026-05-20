5cf91f988f5ffae1849817cb6b8269fd9ef58863
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-19 19:52:51 -0600

    [ADD]: Conexion a la DB de PostgrestSQL

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,23 @@
-export default function sumar () {
-  console.log(2 + 2);
+import express  from "express";
+import router from "./router";
+import db from "./config/db";
+
+// ! Conexion a DB
+async function connecDB() {
+  try {
+    await db.authenticate()
+    db.sync() // * En caso de que vayamos creando nuevos modelos, nuevas columnas a nuestra DB, las agrega
+    console.log('Conexión Exitosa a la DB');
+  } catch (error) {
+    console.log(error);
+    console.log('Hubo un error al conectar la DB');
+    
+  }
 }
+connecDB()
+
+const server = express() // * Sobre la const server agregamos toda la conf del proyecto
+
+server.use('/products', router) // * Ejecuta todos los request con el router
+
+export default server
