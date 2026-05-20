998cf6d1cfb30fbc37bc4d8e566869729a954260
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-19 21:21:55 -0600

    [MODIFY]: Color a los log de Conexion a DB e inicio de Puerto

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import server from "./server";
+import  colors  from "colors";
 
-const port = 4000
+const port = process.env.PORT || 4000
 server.listen(port, () => {
-  console.log(`🚀REST API en el puerto: ${port}🚀`);
+  console.log(colors.bgBlack.white(`🚀REST API en el puerto: ${port}🚀`));
 })
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import express  from "express";
 import router from "./router";
 import db from "./config/db";
+import colors from 'colors'
 
 // ! Conexion a DB
 async function connecDB() {
   try {
     await db.authenticate()
     db.sync() // * En caso de que vayamos creando nuevos modelos, nuevas columnas a nuestra DB, las agrega
-    console.log('Conexión Exitosa a la DB');
+    console.log(colors.blue('Conexión Exitosa a la DB'));
   } catch (error) {
     console.log(error);
-    console.log('Hubo un error al conectar la DB');
+    console.log(colors.red.bold('Hubo un error al conectar la DB'));
     
   }
 }
