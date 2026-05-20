0e9e49be0a091126d21e218684c53155ae5bf5fe
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-20 10:30:37 -0600

    [ADD]: Se creo el Modelo en ORM para Tabla Products

---
+++ src/models/Produc.model.ts
@@ -0,0 +1,30 @@
+// ! DECORADORES:
+import { Table, Column, Model, DataType, Default } from 'sequelize-typescript'
+
+@Table({ // * Decorador de Table
+  tableName: 'product'
+})
+
+// ! Model - Es una Clase que podemos Heredar y en esa clase reescribir y definir nuestros modelos
+class Product extends Model {
+  // * El ID normalmente lo otogan las DB
+  // @Column // ! Decorador campo 2 de DB
+  @Column({
+    // ! Configuracion de las columnas en la DB
+    type: DataType.STRING(100) // * Similar a VARCHAR
+  }) // ! Decorador campo 2 de DB
+    // @Default('') // * VALOR PRESENTE en caso de no añadir nada
+    name: string  // * ESQUEMA
+
+  @Column({
+    type: DataType.FLOAT(6, 2)
+  })
+    price: number // * ESQUEMA
+
+  @Column({
+    type: DataType.BOOLEAN
+  })
+    availability: boolean // * ESQUEMA
+}
+
+export default Product
