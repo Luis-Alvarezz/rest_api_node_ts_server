d4f198d62aefead22c5b4f2638d99902d8b17c68
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-22 17:43:53 -0600

    [ADD]: Validacion en el handler para DB

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,7 @@
 // * Funciones para ayudar a separar la funcionalidad
 import { Request, Response } from "express";
 import Product from "../models/Produc.model";
+import { check, validationResult } from "express-validator";
 
 export const createProduct = async (req: Request, res: Response) => { // * siempre que interactuamos con el modelo, las funciones deben ser ASINCRONAS
   // res.json('Desde POST en handlers/product');
@@ -9,6 +10,21 @@ export const createProduct = async (req: Request, res: Response) => { // * siemp
   // const product = new Product(req.body) // * 1. Creamos el la instancia para formar el Objeto
   // const savedProduct = await product.save() // * 2. Almacenamos objeto en la DB
   // res.json({data: savedProduct}) // * Retornamos el producto desde la DB
+
+  // ! Validacion
+  await check('name').notEmpty().withMessage('El nombre del Producto no puede ir vacio').run(req)
+  await check('price')
+    .isNumeric().withMessage('Valor No valido, debe ser numerico')
+    .notEmpty().withMessage('El precio del Producto no puede ir vacio')
+    .custom( (value) => value > 0 ).withMessage('El precio del Producto debe ser mayor a 0')
+    .run(req)
+
+  // * Leer los mensajes de error
+  let errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()})
+  }
+
   // ! Crear y Almacenar Instancia de body Opcion 2:
   const product = await Product.create(req.body) // * 1. Creamos el instancia y almacena en la DB
   res.json({data: product}) // * Retornamos el producto desde la DB
