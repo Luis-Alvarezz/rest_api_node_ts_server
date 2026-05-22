c327ddb74a234cbb896b73527a74ea106e55e97b
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-22 16:06:18 -0600

    [ADD]: Creacion de Instancia para Objeto y almacenando en la DB

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -6,10 +6,10 @@ export const createProduct = async (req: Request, res: Response) => { // * siemp
   // res.json('Desde POST en handlers/product');
   // console.log(req.body); // * Mostrar la inf en terminal habilitando el server.use(express.json())
   // ! Crear y Almacenar Instancia de body Opcion 1:
-  // const product = new Product(req.body) // * 1. Creamos el Objeto
+  // const product = new Product(req.body) // * 1. Creamos el la instancia para formar el Objeto
   // const savedProduct = await product.save() // * 2. Almacenamos objeto en la DB
   // res.json({data: savedProduct}) // * Retornamos el producto desde la DB
   // ! Crear y Almacenar Instancia de body Opcion 2:
-  const product = await Product.create(req.body) // * 1. Creamos el Objeto
+  const product = await Product.create(req.body) // * 1. Creamos el instancia y almacena en la DB
   res.json({data: product}) // * Retornamos el producto desde la DB
 }
