77db5c1f109ca353739499f0dc3b492343dadffe
Luis-Alvarezz <luisangelug10@gmail.com>
2026-05-27 23:08:34 -0600

    [MODIFY]: Validacion de json response de endpoint /api

--- src/__test__/server.test.ts
+++ src/__test__/server.test.ts
@@ -1,12 +1,17 @@
-// * Primer prueba
-describe('Nuestro primer test', () => {
-  // * Agregar Pruebas de Forma INDIVIDUAL con 'test' o 'it' (es un alia de test)
-  it('Debe revisar que 1 + 1 sean 2', () => {
-    expect(1+1).toBe(2)
-  })
+import request from 'supertest' // * Enviar request, peticion a determinado Endpoint
+import server from '../server'
 
-  it('Debe revisar que 1 + 1 no sean 3', () => {
-    expect(1+1).not.toBe(3)
+describe('GET /api', () => {
+  // * Pruebas aquí
+  it('Should send back a json response', async() => {
+    const response = await request(server).get('/api')
+    // console.log(response);
+    expect(response.status).toBe(200) // * Exista el servidor con res 200
+    expect(response.header['content-type']).toMatch(/json/) // * El contenido debe ser JSON
+    // console.log(response.status);
+    // console.log(response.header['content-type']); // * Salida:  application/json; charset=utf-8  
+    // console.log(response.text); // * Opcion 1.-Muestra Contenido de en Endpoint | NO Se coneta al endpoint
+    // console.log(response.body.msg); // * Opcion 2.-Muestra Contenido de en Endpoint, actua como req.body de Express | Se coneta al endpoint
+    expect(response.body.msg).toBe('Desde API en server.ts') // * Debe ser EXACTO a la res de la API
   })
-
-}) // * Metodo ya importado con Jest
+})
