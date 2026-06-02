6f891944e9048f00bf179bcaffd06d1575371150
Luis-Alvarezz <luisangelug10@gmail.com>
2026-06-01 20:56:14 -0600

    [MODIFY]: Exportacion de conexion a DB en index junto puerto

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import server from "./server";
+import server, { connectDB } from "./server";
 import  colors  from "colors";
+
+connectDB()
 
 const port = process.env.PORT || 4000
 server.listen(port, () => {
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import db from "./config/db";
 import colors from 'colors'
 
 // ! Conexion a DB
-async function connecDB() {
+export async function connectDB() {
   try {
     await db.authenticate()
     db.sync() // * En caso de que vayamos creando nuevos modelos, nuevas columnas a nuestra DB, las agrega
@@ -15,7 +15,7 @@ async function connecDB() {
     
   }
 }
-connecDB()
+// connectDB()
 
 // ! Instancia de Express
 const server = express() // * Sobre la const server agregamos toda la conf del proyecto
